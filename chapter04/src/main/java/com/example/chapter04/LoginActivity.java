41957c7a51d73e829c42d69b83d91e794e63416f
mapleleaf-jian <1028517695@qq.com>
2022-10-13 18:04:06 +0800

    显示Intent

--- chapter04/src/main/java/com/example/chapter04/LoginActivity.java
+++ chapter04/src/main/java/com/example/chapter04/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.chapter04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +19,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         // 创建意图对象，准备跳转到指定的活动页面
-        Intent intent = new Intent(this, SecondActivity.class);
+        // Intent intent = new Intent(this, SecondActivity.class);
+        Intent intent = new Intent();
+//        intent.setClass(this, SecondActivity.class);
+        ComponentName component = new ComponentName(this, SecondActivity.class);
+        intent.setComponent(component);
         // 设置启动标志，跳转到新页面时，栈中原有的实例被清空，同时开辟新任务的活动栈
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
